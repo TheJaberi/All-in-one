3e346e5733e88a4db3da52508009db7d9e077a9a
ajaberi <adnan.ajaberi@gmail.com>
2024-08-31 22:57:08 +0300

    fixed customer profile

--- netfix/views.py
+++ netfix/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 
-from users.models import User, Company
-from services.models import Service
+from users.models import User, Company, Customer
+from services.models import Service, Request_service
 
 
 def home(request):
     return render(request, 'users/home.html', {'user': request.user})
 
 
-def customer_profile(request):
+def customer_profile(request, name):
     # fetches the customer user and all of the services available by it
-    user = User.objects.get(username=request.user)
-    services = Service.objects.filter(customer=user).order_by("-date")
-
+    user = User.objects.get(username=name)
+    services = Request_service.objects.filter(
+        customer=Customer.objects.get(user=user)).order_by("-date")
     return render(request, 'users/profile.html', {'user': user, 'services': services})
 
 def company_profile(request, name):
